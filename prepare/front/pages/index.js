6cc6426ffc3ae3399c8bb96f7b64ca0e3ece80ca
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-03-28 21:06:16 +0900

    feat: 모든 page에 AppLayout 적용

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,7 +1,10 @@
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
   return (
-    <div>Hello, Next!</div>
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
   )
 }
 
