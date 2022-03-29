3c7acd9ecb811854c2fc095438c1ef82cf242a45
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-03-29 22:06:12 +0900

    feat: 더미 데이터를 이용한 로그인 기능 추가

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -15,22 +15,22 @@ const AppLayout = ({ children }) => {
   return (
     <div>
       <Menu mode="horizontal">
-        <Menu.Item>
+        <Menu.Item key="node-bird">
           <Link href="/"><a>노드버드</a></Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="profile">
           <Link href="/profile"><a>프로필</a></Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="search-input">
           <SearchInput enterButton/>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="signup">
           <Link href="/signup"><a>회원가입</a></Link>
         </Menu.Item>
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {isLoggedIn ? <UserProfile/> : <LoginForm/>}
+          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn}/> : <LoginForm setIsLoggedIn={setIsLoggedIn}/>}
         </Col>
         <Col xs={24} md={12}>
           {children}
--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -6,8 +6,11 @@ import styled from 'styled-components';
 const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
 
-const LoginForm = () => {
+const LoginForm = ({ setIsLoggedIn }) => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
 
@@ -19,8 +22,13 @@ const LoginForm = () => {
     setPassword(e.target.value);
   },[]);
 
+  const onSubmitForm = useCallback(()=>{
+    console.log(id, password);
+    setIsLoggedIn(true);
+  }, [id, password]);
+
   return (
-    <Form>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br/>
@@ -45,7 +53,7 @@ const LoginForm = () => {
         <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
         <Link href="/signup"><a><Button>회원가입</Button></a></Link>
       </ButtonWrapper>
-    </Form>
+    </FormWrapper>
   );
 };
 
