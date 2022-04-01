a7c8789e279340978b07a151637c29d900beac78
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-04-01 22:55:48 +0900

    feat: 미들웨어 및 리덕스 데브툴즈 추가

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,13 +1,14 @@
 import { createWrapper } from 'next-redux-wrapper';
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 
 const configureStore = () => {
-  const store = createStore(reducer);
-  store.dispatch({
-    type: 'CHANGE_NICKNAME',
-    data: 'jinu',
-  })
+  const middlewares = [];
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(applyMiddleware(...middlewares))
+  const store = createStore(reducer, enhancer);
   return store;
 };
 
