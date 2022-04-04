e2ce63301f1551b8e965096668862a5fbd8e9b54
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-04-04 21:36:12 +0900

    feat: 댓글 기능 추가

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import { useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { Button, Card, Popover } from 'antd';
+import { Button, Card, Popover, Avatar, List, Comment } from 'antd';
 import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, HeartTwoTone } from '@ant-design/icons';
-import Avatar from 'antd/lib/avatar/avatar';
 import PostImages from './PostImages';
+import CommentForm from './CommentForm';
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
@@ -53,11 +53,23 @@ const PostCard = ({ post }) => {
       </Card>
       {commentFormOpened && (
         <div>
-          댓글 부분
+          <CommentForm post={post} />
+          <List 
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout='horizontal'
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}   
+                  content={item.content}             
+                />
+              </li>
+            )}
+          />
         </div>
       )}
-      {/* <CommentForm /> */}
-      {/* <Comments /> */}
     </div>
   );
 };
