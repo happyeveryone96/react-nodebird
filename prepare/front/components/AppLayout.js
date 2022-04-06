c89a8244eef691a8f260dc32b0f21b602a5ec047
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-04-06 21:13:36 +0900

    feat: 이미지 캐러셀 추가

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
 
@@ -10,11 +10,27 @@ const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `;
 
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+
+  .ant-col:first-child {
+    padding-left: 0 !important;
+  }
+
+  .ant-col:last-child {
+    padding-right: 0 !important;
+  }
+`;
+
 const AppLayout = ({ children }) => {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
   return (
     <div>
+      <Global/>
       <Menu mode="horizontal">
         <Menu.Item key="node-bird">
           <Link href="/"><a>노드버드</a></Link>
