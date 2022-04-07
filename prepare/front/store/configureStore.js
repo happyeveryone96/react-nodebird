421a09f268122ac0b6c10ed5282d44234d20e0e5
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-04-07 22:27:47 +0900

    chore: redux-saga 설치 및 초기 설정

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -2,13 +2,17 @@ import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sages';
 
 const configureStore = () => {
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares))
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
