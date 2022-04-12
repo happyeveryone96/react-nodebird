19c922a92829237164b7905404f1db8d76e21d9b
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-04-12 21:27:19 +0900

    refactor: 코드 정리 및 eslint 설정 변경

--- prepare/front/components/PostForm.js
+++ prepare/front/components/PostForm.js
@@ -1,20 +1,26 @@
-import { useCallback, useState, useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { Form, Input, Button } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { addPost } from '../reducers/post';
+import useInput from '../hooks/useInput';
 
 const PostForm = () => {
-  const { imagePaths } = useSelector((state) => state.post);
+  const { imagePaths, addPostDone } = useSelector((state) => state.post);
   const dispatch = useDispatch();
-  const imageInput = useRef();
-  const [text, setText] = useState('');
-  const onChangeText = useCallback((e) => {
-    setText(e.target.value);
-  }, [])
+  const [text, onChangeText, setText] = useInput('');
+
+  useEffect(() => {
+    if (addPostDone) {
+      setText('');
+    }
+  }, [addPostDone])
+
   const onSubmit = useCallback(() => {
-    dispatch(addPost);
-    setText('');
-  }, []);
+    dispatch(addPost(text));
+  }, [text]);
+
+  const imageInput = useRef();
+
   const onClickImageUpload = useCallback(() => {
     imageInput.current.click();
   }, [imageInput.current])
--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -4,13 +4,17 @@ import Head from 'next/head';
 import { Form, Input, Checkbox, Button } from 'antd';
 import useInput from '../hooks/useInput';
 import styled from 'styled-components';
+import { SIGN_UP_REQUEST } from '../reducers/user';
+import { useDispatch, useSelector } from 'react-redux';
 
 const ErrorMessage = styled.div`
   color: red;
 `;
 
 const Signup = () => {
-  const [id, onChangeId] = useInput('');
+  const dispatch = useDispatch();
+  const { signUpLoading } = useSelector((state) => state.user);
+  const [email, onChangeEmail] = useInput('');
   const [nickname, onChangeNickname] = useInput('');
   const [password, onChangePassword] = useInput('');
 
@@ -34,8 +38,12 @@ const Signup = () => {
     if (!term) {
       return setTermError(true);
     }
-    console.log(id, nickname, password)
-  },[password, passwordCheck, term]);
+    console.log(email, nickname, password);
+    dispatchEvent({
+      type: SIGN_UP_REQUEST,
+      data: { email, password, nickname },
+    });
+  },[email, password, passwordCheck, term]);
   return (
     <AppLayout>
       <Head>
@@ -43,9 +51,9 @@ const Signup = () => {
       </Head>
       <Form onFinish={onSubmit}>
         <div>
-          <label htmlFor="user-id">아이디</label>
+          <label htmlFor="user-email">이메일</label>
           <br/>
-          <Input name="user-id" value={id} required onChange={onChangeId}/>
+          <Input name="user-email" type="email" value={email} required onChange={onChangeEmail}/>
         </div>
         <div>
           <label htmlFor="user-nick">닉네임</label>
@@ -74,7 +82,7 @@ const Signup = () => {
           {termError && <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>}
         </div>
         <div style={{ marginTop: 10}}>
-          <Button type="primary" htmlType="submit">가입하기</Button>
+          <Button type="primary" htmlType="submit" loading={signUpLoading}>가입하기</Button>
         </div>
       </Form>
     </AppLayout>
